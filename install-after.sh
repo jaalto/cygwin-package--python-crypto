03f91361b4b56c124d3a4f74bd3b1a09eab08fb9
Jari Aalto <jari.aalto@cante.net>
2010-06-19 02:26:21 +0300

    import original  CYGWIN-PATCHES.git 2010-06-18

---
+++ install-after.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+# Copyright (C) 2007 Jari Aalto; Licenced under GPL v2 or later
+#
+# install-after.sh -- Custom installation
+#
+# The script will receive one argument: relative path to
+# installation root directory. Script is called like:
+#
+#    $ install-after.sh .inst
+#
+# This script is run after [install] command.
+
+Cmd()
+{
+    echo "$@"
+    [ "$test" ] && return
+    "$@"
+}
+
+Main()
+{
+    root=${1:-".inst"}
+
+    if [ "$root"  ] && [ -d $root ]; then
+
+        root=$(echo $root | sed 's,/$,,')  # Delete trailing slash
+	bin=$root/usr/bin
+
+        if [ -d $bin ]; then
+            echo ">> Remove empty dir"
+            Cmd rmdir $bin
+        fi
+    fi
+}
+
+Main "$@"
+
+# End of file
